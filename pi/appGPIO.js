7de83e1169e3f8dad1efd771a6eb181a8d5af6c8
mrendleman <michael-rendleman@uiowa.edu>
2020-04-21 20:15:46 +0100

    some LED boilerplate

--- pi/appGPIO.js
+++ pi/appGPIO.js
@@ -1,2 +1,4 @@
 var firebase = require('firebase/app');
 require('firebase/database');
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4,'out');
--- pi/appSense.js
+++ pi/appSense.js
@@ -1,2 +1,5 @@
 var firebase = require('firebase/app');
 require('firebase/database');
+var nodeimu = require('@trbll/nodeimu');
+var IMU = new nodeimu.IMU();
+var sense = require('@trbll/sense-hat-led'):
