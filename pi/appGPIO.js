221e5d8b26ca7901c7297e43dae3176195f08ca6
mrendleman <michael-rendleman@uiowa.edu>
2020-04-22 16:04:55 +0100

    pin-based LED control figured out

--- pi/appGPIO.js
+++ pi/appGPIO.js
@@ -1,4 +1,7 @@
 var firebase = require('firebase/app');
 require('firebase/database');
 var Gpio = require('onoff').Gpio;
-var LED = new Gpio(4,'out');
+var LED = new Gpio(4,'out'); // LED is in pin 4 for GPIO
+
+// to turn on LED: LED.writeSync(1);
+// to turn off LED: LED.writeSync(0);
