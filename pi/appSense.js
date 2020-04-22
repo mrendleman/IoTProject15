9621b9efdd098bd442a6ece13afc9a15bbb58a9b
mrendleman <michael-rendleman@uiowa.edu>
2020-04-22 16:43:45 +0100

    code for SENSE hat firebase connection written (untested, I don't have a sensehat board)

--- pi/appSense.js
+++ pi/appSense.js
@@ -4,3 +4,6 @@ var nodeimu = require('@trbll/nodeimu');
 var IMU = new nodeimu.IMU();
 var sense = require('@trbll/sense-hat-led'):
 var firebaseConfig = require("./firebaseConfig.json");
+
+firebase.intiializeApp(firebaseConfig);
+var database = firebase.database();
