6aed4e10424388ff4c58155dbc6f26c2ea145079
lcastrogrc <lcastrogarcia@uiowa.edu>
2020-04-23 16:05:20 +0000

    commit message

--- pi/appSense.js
+++ pi/appSense.js
@@ -17,28 +17,19 @@ var updateTouchRef = database.ref('lorena');
 updateTouchRef.on('value', function(snapshot) {
 	if (snapshot.val()==true) {
 		console.log(snapshot.val());
-		database.ref().once('value').then(function(dataSnapshot) {
-			setPixelValue([255,99,71]);
-			//database.ref('lorena').set(false);
-		});
+		setPixelValue([255,99,71]);
 	}else{
 		console.log(snapshot.val());
-		database.ref().once('value').then(function(dataSnapshot) {
-			setPixelValue([0,128,8]);
-			//database.ref('lorena').set(false);
-		});
+		setPixelValue([0,128,8]);
 	}
 });
 
 var idx = [0, 1, 2, 3, 4, 5, 6, 7];
 function setPixelValue(color) {
-	//console.log("Setting light %d, %d to RGB value [ %d, %d, %d ]",row,col,rgb[0],rgb[1],rgb[2]);
-	
 	for (var i = 0; i < idx.length; i++) {
     		for (var j = 0; j < idx.length; j++) {
     			sense.setPixel(i,j,color);
 		}
 	}
 	
-	//sense.clear(color);
 }
